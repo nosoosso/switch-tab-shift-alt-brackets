49832d4e317685e0b9b8e180f6e28a9115300016
nosoosso <nosoosso.null@gmail.com>
2017-11-28 01:21:00 +0900

    move functions to background.js

--- src/lib/background.js
+++ src/lib/background.js
@@ -1,17 +1,37 @@
-browser.runtime.onMessage.addListener(handleMessage);
+const KEY_SHIFT = 16;
+const KEY_ALT = 18;
+const KEY_OPENING_BRACKET = 219; // [
+const KEY_CLOSING_BRACKET = 221; // ]
 
-function handleMessage(message) {
-  switch (message) {
-    case 'previousTab': {
-      previousTab();
-      break;
-    }
-    case 'nextTab': {
-      nextTab();
-      break;
-    }
-    default: {
-    }
+
+const keyConfigs = [{
+  keys: [KEY_SHIFT, KEY_ALT, KEY_OPENING_BRACKET],
+  action: previousTab,
+}, {
+  keys: [KEY_SHIFT, KEY_ALT, KEY_CLOSING_BRACKET],
+  action: nextTab,
+}];
+
+
+const activeKeyMap = {};
+
+function setKeyDown(keyCode) {
+  activeKeyMap[keyCode] = true;
+}
+
+function setKeyUp(keyCode) {
+  activeKeyMap[keyCode] = false;
+}
+
+function doAction(activeKeyMap, keyConfigs) {
+  const matchedConfig = keyConfigs.find((conf) => {
+    return conf.keys.every((keyCode) => {
+      return activeKeyMap[keyCode] === true;
+    });
+  });
+
+  if (matchedConfig) {
+    matchedConfig.action();
   }
 }
 
@@ -60,3 +80,22 @@ function nextTab() {
       console.log(e);
     });
 }
+
+function handleMessage(message) {
+  switch (message.type) {
+    case 'keydown': {
+      setKeyDown(message.keyCode);
+      doAction(activeKeyMap, keyConfigs);
+      break;
+    }
+    case 'keyup': {
+      setKeyUp(message.keyCode);
+      break;
+    }
+    default: {
+    }
+  }
+}
+
+
+browser.runtime.onMessage.addListener(handleMessage);
